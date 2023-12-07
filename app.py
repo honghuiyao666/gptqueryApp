33bc4ca0f65f30eabd2f925ff387680574f1634e
honghuiyao666 <153155001+honghuiyao666@users.noreply.github.com>
2023-12-06 22:48:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import mysql.connector
 
 # Add your open AI API key
-openai.api_key = open_ai_api_key
+openai.api_key = st.secrets.open_ai_api_key
 
 # Setting up database attributes
 # Add your server url
